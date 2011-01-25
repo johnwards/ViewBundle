67c8535c8664552f46edea39f6ff00c388115056
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-01-25 09:32:01 +0100

    initial support for using the serializer component

--- View/DefaultView.php
+++ View/DefaultView.php
@@ -7,7 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Bundle\FrameworkBundle\Templating\Loader\TemplateParser;
+
+use Symfony\Component\Serializer\Encoder\TemplatingAwareEncoderInterface;
 
 /*
  * This file is part of the Symfony framework.
@@ -34,6 +35,7 @@ class DefaultView
     protected $container;
     protected $globalParameters;
     protected $customHandlers = array();
+    protected $serializer;
 
     protected $redirect;
     protected $template;
@@ -121,6 +123,10 @@ class DefaultView
 
     public function getTemplate()
     {
+        if (null === $this->template) {
+            return null;
+        }
+
         $template = $this->template;
 
         if (empty($template['format'])) {
@@ -207,11 +213,7 @@ class DefaultView
             $callback = $this->customHandlers[$format];
             $response = call_user_func($callback, $this, $request, $response);
         } else {
-            $method = 'handle'.ucfirst($format);
-            if (!method_exists($this, $method)) {
-                throw new NotFoundHttpException("Format '$format' not supported, handler must be implemented");
-            }
-            $response = $this->$method($request, $response);
+            $response = $this->transform($request, $response, $format, $this->getTemplate());
         }
 
         $this->reset();
@@ -231,221 +233,63 @@ class DefaultView
     }
 
     /**
-     * Generic handler for default formats
+     * Reset serializer service
+     */
+    protected function resetSerializer()
+    {
+        $this->serializer = null;
+    }
+
+    /**
+     * Get the serializer service, add encoder in case its not yet set for the passed format
      *
-     * Handles redirects, or calls a "transform{$format}" method to transform the parameters into a response content
+     * @param string $format
+     *
+     * @return Symfony\Component\Serializer\SerializerInterface
+     */
+    protected function getSerializer($format = null)
+    {
+        if (null === $this->serializer) {
+            $this->serializer = $this->container->get('serializer');
+        }
+
+        if (null !== $format && !$this->serializer->supports($format)) {
+            $this->serializer->addEncoder($format, $this->container->get('encoder.'.$format));
+        }
+
+        return $this->serializer;
+    }
+
+    /**
+     * Generic transformer
+     *
+     * Handles redirects, or transforms the parameters into a response content
      *
      * @param Request $request
      * @param Response $response
      * @param string $format
+     * @param string $template
      *
      * @return Response
      */
-    protected function handleGeneric(Request $request, Response $response, $format)
+    protected function transform(Request $request, Response $response, $format, $template)
     {
         if ($this->redirect) {
             $response->setRedirect($this->redirect['location'], $this->redirect['status_code']);
             return $response;
         }
 
-        $method = 'transform'.$format;
-        if (!method_exists($this, $method)) {
-            throw new NotFoundHttpException('Format '.$format.' not supported, transformer must be implemented');
+        $serializer = $this->getSerializer($format);
+        $encoder = $serializer->getEncoder($format);
+
+        if ($encoder instanceof TemplatingAwareEncoderInterface) {
+            $encoder->setTemplate($template);
         }
 
-        $content = $this->$method($request, $response, $this->parameters);
+        $parameters = (array)$this->parameters;
+        $content = $serializer->serialize($parameters, $format);
 
         $response->setContent($content);
         return $response;
-    }
-
-    /**
-     * Html format handler
-     *
-     * @param Request $request
-     * @param Response $response
-     *
-     * @return Response
-     */
-    protected function handleHtml(Request $request, Response $response)
-    {
-        return $this->handleGeneric($request, $response, 'html');
-    }
-
-    /**
-     * Html parameter transformer
-     *
-     * Merges the global parameters with the given parameters and then
-     * renders the given template
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param mixed $parameters
-     *
-     * @return string
-     */
-    protected function transformHtml(Request $request, Response $response, $parameters)
-    {
-        $parameters = (array)$parameters;
-        $parameters = array_merge($this->globalParameters, $parameters);
-        return $this->container->get('templating')->render($this->getTemplate(), $parameters);
-    }
-
-    /**
-     * Json format handler
-     *
-     * @param Request $request
-     * @param Response $response
-     *
-     * @return Response
-     */
-    protected function handleJson(Request $request, Response $response)
-    {
-        return $this->handleGeneric($request, $response, 'json');
-    }
-
-    /**
-     * Json parameter transformer
-     *
-     * json encodes the parameters given to it, objects that do not implement
-     * toArray() will only have their public properties present in the json
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param mixed $parameters
-     *
-     * @return string
-     */
-    protected function transformJson(Request $request, Response $response, $parameters)
-    {
-        $parameters = (array)$parameters;
-        return json_encode($this->parametersToArray($parameters));
-    }
-
-    /**
-     * Xml format handler
-     *
-     * @param Request $request
-     * @param Response $response
-     *
-     * @return Response
-     */
-    protected function handleXml(Request $request, Response $response)
-    {
-        return $this->handleGeneric($request, $response, 'xml');
-    }
-
-    /**
-     * Xml parameter transformer
-     *
-     * converts the parameters given to it into an XML tree, objects that do not
-     * implement toArray() or Traversable will be represented as empty nodes
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param mixed $parameters
-     *
-     * @return string
-     */
-    protected function transformXml(Request $request, Response $response, $parameters)
-    {
-        if ($parameters instanceof \DOMDocument) {
-            $dom = $parameters;
-        } else if (is_string($parameters) && !empty($parameters)) {
-            $dom = new \DOMDocument();
-            $dom->loadXML($parameters);
-        } else if (is_array($parameters)) {
-            $dom = new \DOMDocument();
-            $dom->loadXML("<response/>");
-            $this->parametersToDom($parameters, $dom, $dom->documentElement);
-        } else {
-            throw new \UnexpectedValueException('Unsupported type '.gettype($parameters));
-        }
-
-        return $dom->saveXML();
-    }
-
-    /**
-     * Recursively converts all objects in a parameter array into arrays
-     *
-     * Only objects implementing the toArray() method will be converted,
-     * the others are left intact
-     *
-     * @param array $parameters
-     *
-     * @return array
-     */
-    public function parametersToArray(array $parameters)
-    {
-        foreach ($parameters as $key => $value) {
-            if (is_object($value) && method_exists($value, 'toArray')) {
-                $parameters[$key] = $this->parametersToArray($value->toArray());
-            } elseif (is_array($value)) {
-                $parameters[$key] = $this->parametersToArray($value);
-            }
-        }
-        return $parameters;
-    }
-
-    /**
-     * Converts an array to an XML DOM structure.
-     *
-     * @param mixed $data The array or traversable object to convert.
-     * @param DOMDocument $domdoc The document into which to insert the
-     *        result. The document must already have been created in memory.
-     * @param DOMNode $domnode The element in $domdoc to which the result is added.
-     * @param array $keys An array of key names, used internally for recursion
-     * @param string $parentKey The key of the parent node, used internally for recursion
-     */
-    public function parametersToDom($data, $domdoc, $domnode, $keys = array(), $parentKey = null)
-    {
-        foreach ($data as $nodeKey => $node) {
-            $nodeParentKey = $nodeKey;
-            if (isset($keys[$parentKey])) {
-                $key = $keys[$parentKey];
-            } else {
-                $key = 'entry';
-            }
-
-            $nodeKey = (is_numeric($nodeKey)) ? $key : $nodeKey;
-
-            $elem = $domdoc->createElement($nodeKey);
-            if ($elem instanceof \DOMNode) {
-                if ($nodeKey === $key) {
-                    $elem->setAttribute('key', $nodeParentKey);
-                }
-
-                if (is_array($node)) {
-                    $this->parametersToDom($node, $domdoc, $elem, $keys, $nodeParentKey);
-                } elseif (method_exists($node, 'toArray')) {
-                    $this->parametersToDom($node->toArray(), $domdoc, $elem, $keys, $nodeParentKey);
-                } elseif ($node instanceof \Traversable) {
-                    $this->parametersToDom($node, $domdoc, $elem, $keys, $nodeParentKey);
-                } else {
-                    if ($node instanceof \DOMDocument){
-                        $nodeObj = $domdoc->importNode($node->documentElement, true);
-                    } elseif (is_bool($node) || null === $node) {
-                        $node = strtolower(var_export($node, true));
-                        $nodeObj = $domdoc->createTextNode($node);
-                    } elseif (!is_object($node)) {
-                        $nodeObj = $domdoc->createTextNode($node);
-                    } elseif ($node instanceof \DateTime) {
-                        $nodeObj = $domdoc->createTextNode($node->format(DATE_ISO8601));
-                    } else {
-                        $nodeObj = $domdoc->createTextNode('');
-                    }
-
-                    if (isset($nodeObj) && $nodeObj instanceof \DOMNode) {
-                        $elem->appendChild($nodeObj);
-                    }
-                }
-
-                if (null !== $domnode) {
-                    $domnode->appendChild($elem);
-                } else {
-                    $domdoc->appendChild($elem);
-                }
-            }
-        }
     }
 }
